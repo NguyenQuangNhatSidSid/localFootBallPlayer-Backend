d14f8bf07b76cb10f713d44e0c4349e785ecd3d9
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-04-23 23:55:22 +0700

    add 4 router delete,getGameByRank , GetOnegame,update

--- routers/game.router.js
+++ routers/game.router.js
@@ -5,5 +5,9 @@ const gameController = require("../controllers/game.controller");
 // router.get("/", productController.getProduct);
 router.post("/game", gameController.getGame);
 router.post("/game/create-game", gameController.createGames);
+router.post("/game/find-one-game/:id", gameController.getOneGame);
+router.post("/game/update-game/:id", gameController.updateGame);
+router.post("/game/delete-game/:id", gameController.deleteGame);
+router.post("/game/get-game-by-rank", gameController.getGameByRank);
 
 module.exports = router;
